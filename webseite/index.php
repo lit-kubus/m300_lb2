d1031dccc5baa3383202c1fcc190586d432af8ac
Belchior <jonathan.belchior02@gmail.com>
2021-03-12 16:18:46 +0100

    Funktion für Abfrage funktioniert auf Webseite (Name und Wert der Aktie)

--- webseite/index.php
+++ webseite/index.php
@@ -4,18 +4,18 @@
 
     </head>
     <body>
+        <?php 
+            $valor = $_POST['stock'];
+            $price = exec("python ./python/prices.py $valor");
+            $value_price = file_get_contents("./python/tmp/prices.txt");
+            $name_stock = file_get_contents("./python/tmp/stock.txt");
+            echo $value_price .$name_stock; 
+        ?>
         <form action="" method="Post">
             <label for="stock">Stock:</label><br>
             <input type="text" name="stock" value="" ><br><br>
             <input type="submit" name="submit" value="Submit">
         </form>
-        <?php 
-            function pre_r( $array )
-            {
-                $valor = $_POST['stock'];
-                $price = exec("python ./python/prices.py $valor 2<&1");
-                sleep (5);
-            }
-        ?>
+    
     </body>
 </html>
