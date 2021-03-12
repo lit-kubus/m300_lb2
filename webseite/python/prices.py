4a3a39e88eccb132ff3ed04100b016bc6092e80b
Belchior <jonathan.belchior02@gmail.com>
2021-03-12 15:25:30 +0100

    Python Script persönlich anpassen (Ordner)

--- webseite/python/prices.py
+++ webseite/python/prices.py
@@ -5,8 +5,8 @@ stock = str(sys.argv[1])
 
 price = si.get_live_price(stock)
 price_str = str(price)
-file_price = "E:/Users/chris/Schule/Modul_300/m300_lb/website/python/tmp/price.txt"
-file_stock = "E:/Users/chris/Schule/Modul_300/m300_lb/website/python/tmp/stock.txt"
+file_price = "C:/Users/belchior/Modul300/m300_lb2/webseite/python/tmp/prices.txt"
+file_stock = "C:/Users/belchior/Modul300/m300_lb2/webseite/python/tmp/stock.txt"
 
 with open(file_price, 'w') as fileowrite:
         fileowrite.write(price_str+"<br>")
