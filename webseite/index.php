13f82f830fa23ec575f41e13ab56f4b08853761e
Belchior <jonathan.belchior02@gmail.com>
2021-03-12 15:15:50 +0100

    PHP file machen

---
+++ webseite/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+
+        <form action="" method="Post">
+            <label for="stock">Stock:</label><br>
+            <input type="text" name="stock" value="" ><br><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+
+    </body>
+</html>
