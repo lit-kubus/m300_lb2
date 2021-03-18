fd01b040d807aaae15e4d609be146c189013ba3f
lit-kubus <jonathan.belchior02@gmail.com>
2021-03-18 22:56:23 +0100

    Website in Apache korrekt integrieren

---
+++ Vagrant/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" href="main.css">
+
+</head>
+    <body>
+
+        <div class=title>
+            <h1>STONK Website</h1>
+        </div>
+
+        <div class=price-for>
+        
+        <?php
+            $value = $_POST['stock'];
+            $price = exec("python3.9 ./python/get_price.py $value 2<&1");
+            $value_py = file_get_contents( "./python/tmp/stock.txt" );
+            $price_py = file_get_contents( "./python/tmp/prices.txt" );
+            echo "Price for: " .$value_py ."is: " .$price_py ;
+        ?>
+<>
+        </div>
+        <div class=form>
+            <form action="" method="Post">
+                <label for="stock">Stock:</label><br>
+                <input type="text" name="stock" value="" ><br><br>
+                <input type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+        <div class=footer>
+            <h3>Copyright von lit-kubus</h3>
+        </div>
+    </body>
+</html>
