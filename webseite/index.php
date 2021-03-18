95dbcaa54053c039f59e0754dbb86f4973b395b7
lit-kubus <jonathan.belchior02@gmail.com>
2021-03-18 20:23:46 +0100

    vagrant file upload

--- webseite/index.php
+++ webseite/index.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html><html  >
 <head>
 
+    <link rel="stylesheet" href="main.css">
+
 </head>
     <body>
+
+        <div id=title>
+
+            <p>STONK Website</p>
+
+        </div>
+
+        <div id=price-for>
+        
         <?php
             $valor = $_POST['stock'];
             $price = exec("python3.9 ./python/get_price.py $valor 2<&1");
@@ -10,6 +21,9 @@
             $price_py = file_get_contents( "./python/tmp/prices.txt" );
             echo "Price for: " .$valor_py ."is: " .$price_py ;
         ?>
+
+        </div>
+
         <form action="" method="Post">
             <label for="stock">Stock:</label><br>
             <input type="text" name="stock" value="" ><br><br>
